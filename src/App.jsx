36e63ded164198300c4f9aaad29949355570f9b1
NickBarkerDev <nickthedevguy@gmail.com>
2023-03-15 00:32:57 -0700

    imported bootstrap, added custom styles

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,3 @@
-
 import './App.css';
 import PersonCard from './components/PersonCard';
 
@@ -6,10 +5,12 @@ function App() {
   return (
     <div className="App">
       <h1>People Persons</h1>
-      <PersonCard firstName={"Nick"} lastName={"Barker"} age={33} hairColor={"blonde"} />
-      <PersonCard firstName={"Leo"} lastName={"Nidas"} age={10} hairColor={"spotted"} />
-      <PersonCard firstName={"Richard"} lastName={"Rinkle"} age={69} hairColor={"brown"} />
-      <PersonCard firstName={"Robyn"} lastName={"Banks"} age={42} hairColor={"red"} />
+      <div className="all-people">
+        <PersonCard firstName={"Nick"} lastName={"Barker"} age={33} hairColor={"blonde"} />
+        <PersonCard firstName={"Leo"} lastName={"Nidas"} age={10} hairColor={"spotted"} />
+        <PersonCard firstName={"Richard"} lastName={"Rinkle"} age={69} hairColor={"brown"} />
+        <PersonCard firstName={"Robyn"} lastName={"Banks"} age={42} hairColor={"red"} />
+      </div>
     </div>
   );
 }
